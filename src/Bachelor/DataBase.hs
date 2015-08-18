ab74d0d59e97faa9f07c3ad8f040dd33dcc78779
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-08-18 14:21:34 +0200

    added DBInfo record for keeping track of the db state.

--- src/Bachelor/DataBase.hs
+++ src/Bachelor/DataBase.hs
@@ -1,3 +1,4 @@
+ {-# LANGUAGE OverloadedStrings #-}
 module Bachelor.DataBase where
 
 {-
@@ -5,4 +6,28 @@ module Bachelor.DataBase where
  - a database.
  - -}
 
- --instance IOEventData where
+--instance IOEventData where
+
+import Bachelor.Types
+import Database.PostgreSQL.Simple
+import GHC.RTS.Events
+import qualified Data.HashMap.Strict as M
+
+myConnectInfo :: ConnectInfo
+myConnectInfo = defaultConnectInfo {
+    connectPassword = "bohCh0mu"
+    }
+
+mkConnection = connect myConnectInfo
+
+-- the information necessary for entering events into the database.
+-- because the database should be able to contain multiple traces,
+-- we keep a set of keys to uniquely identify the machines, processes and
+-- threads within the current trace.
+
+data DBInfo = DBInfo {
+    db_machines   :: M.HashMap MachineId Int,
+    db_processes  :: M.HashMap (MachineId,ProcessId) Int,
+    db_threads    :: M.HashMap (MachineId,ProcessId,ThreadId) Int,
+    db_connection :: Connection
+    }
