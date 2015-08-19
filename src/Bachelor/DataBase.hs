afeadf2b0edd7652fa8d2ce94d745963771b7feb
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-08-19 16:36:28 +0200

    adding traces works.

--- src/Bachelor/DataBase.hs
+++ src/Bachelor/DataBase.hs
@@ -12,6 +12,7 @@ import Bachelor.Types
 import Database.PostgreSQL.Simple
 import GHC.RTS.Events
 import qualified Data.HashMap.Strict as M
+import Control.Applicative
 
 myConnectInfo :: ConnectInfo
 myConnectInfo = defaultConnectInfo {
@@ -33,3 +34,23 @@ data DBInfo = DBInfo {
     db_connection :: Connection
     }
 
+-- when starting to parse a new file, we need to create a new connection,
+-- and then insert a new trace into our list of traces, then store the trace_id
+-- into a new DBInfo value.
+
+createDBInfo :: FilePath -> IO ()
+createDBInfo file = do
+    conn <- mkConnection
+    traceId <- head <$> query conn ("Insert Into Traces (filename, creation_date) values( ? , now()) returning trace_id;") (Only file)
+    case traceId of
+        Only id -> putStrLn $ show $ (id :: Int)
+
+-- insertion functions for different Events
+insertEvent :: DBInfo -> Event -> IO DBInfo
+insertEvent dbi (Event ts spec) =
+    case spec of
+        CreateMachine realtime m_id -> do
+            return dbi
+        -- events not implemented
+        _                  -> do
+            return dbi
