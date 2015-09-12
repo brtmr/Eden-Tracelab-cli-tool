3da4e849b5653e743f39c61192deebe37b6a3387
Bastian Reitemeier <Reitemei@students.uni-marburg.de>
2015-09-12 15:25:57 +0200

    added process count test for machines

--- test/Bachelor/SeqParseSpec.hs
+++ test/Bachelor/SeqParseSpec.hs
@@ -16,8 +16,11 @@ spec = do
                 (setProcessState $ addRunningThread $ emptyProcess)^.p_state
                     `shouldBe` Running
         context "ProcessState manipulation with updateThreadCount" $ do
-            it "sum of processes is always equal to p_tTotal" $ property $
-                sumProperty
+            it "sum of threads is always equal to p_tTotal" $ property $
+                sumPropertyProcesses
+        context "MachineState manipulation with updateProcessCount" $ do
+            it "sum of processes is always equal to m_pTotal" $ property  $ do
+                sumPropertyMachines
 
 main :: IO()
 main = hspec spec
@@ -47,6 +50,23 @@ instance Arbitrary ProcessState where
             _p_tTotal    = total
         }
 
+instance Arbitrary MachineState where
+    arbitrary = do
+        state <- arbitrary
+        Positive ts <- arbitrary
+        Positive running <- arbitrary
+        Positive blocked <- arbitrary
+        Positive runnable <- arbitrary
+        return $ MachineState {
+            _m_state     = state,
+            _m_timestamp = ts,
+            _m_pRunning  = running,
+            _m_pRunnable = runnable,
+            _m_pBlocked  = blocked,
+            _m_pTotal    = running + runnable + blocked
+        }
+
+
 emptyProcess = ProcessState {
     _p_parent    = 1,
     _p_state     = Idle,
@@ -57,12 +77,16 @@ emptyProcess = ProcessState {
     _p_tTotal    = 0
     }
 
-addRunningThread :: ProcessState -> ProcessState
 addRunningThread p = updateThreadCount p Nothing (Just Running)
 
 {- QuickCheck Property: updating the thread count should always leave
  - the sum p_tTotal equal to the sum of the threads -}
-sumProperty pstate s1 s2 =
+sumPropertyProcesses :: ProcessState -> (Maybe RunState) -> (Maybe RunState) -> Bool
+sumPropertyProcesses pstate s1 s2 =
     let e = updateThreadCount pstate s1 s2
     in e^.p_tTotal == (e^.p_tRunning + e^.p_tBlocked + e^.p_tRunnable)
 
+sumPropertyMachines :: MachineState -> (Maybe RunState) -> (Maybe RunState) -> Bool
+sumPropertyMachines mState s1 s2 =
+    let e = updateProcessCount mState s1 s2
+    in _m_pTotal e == (_m_pRunning e + _m_pBlocked e + _m_pRunnable e)
